50c3c02b9bf253aed1daec1871c0cb13da9ad268
khoaitayzhieng <duchtse170405@fpt.edu.vn>
2026-02-25 09:42:04 +0700

    add dechoat

---
+++ Assets/Scripts/QuestUIManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class QuestUIManager : MonoBehaviour
+{
+    public GameObject questPanel;
+
+    public void ToggleQuestPanel()
+    {
+        questPanel.SetActive(!questPanel.activeSelf);
+    }
+}
